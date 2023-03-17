6ebb7076b15e1d7626e029e8a821f6ccf8183a0f
Damilare Adeyera <adeyeradamilare361997@gmail.com>
2023-03-17 12:43:33 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-  main - A program that print a piece of art using printf function
-return: 0 (success)
+*main - Entry point
+*Description:  A program that print a piece of art using printf function
+*return: 0 (success)
 */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+Return: 0 (success)
 }
 
