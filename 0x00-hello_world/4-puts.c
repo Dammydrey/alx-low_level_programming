c7fd65eb5b3c709598e1a28cf8ca0d6f7e254dee
Damilare Adeyera <adeyeradamilare361997@gmail.com>
2023-03-17 05:52:11 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  main - puts "programing without prints but with puts"
+Return: 0 (success)
+*/
+init main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
