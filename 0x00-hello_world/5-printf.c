b1b2c11c717fe943bfa68d597598e2f8111dbe07
Damilare Adeyera <adeyeradamilare361997@gmail.com>
2023-03-18 01:49:35 -0700

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+
 /**
-*main - Entry point
-*Description: A program that prints using the puts function
-*return: 0 (success)
+* main - Entry point
+*
+* Description: 'A program that prints using the printf function'
+*
+* return: always 0 (success)
 */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-Return: 0 (success)
+return (0);
 }
 
