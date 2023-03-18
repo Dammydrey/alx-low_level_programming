4f5e30e017771ea02bf18eaf9d1c1ea7459b9160
Damilare Adeyera <adeyeradamilare361997@gmail.com>
2023-03-18 13:50:28 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Description: 'A program that prints using the printf function'
-*
-* return: always 0 (success)
+*main - Entry point
+*Description: 'A program that prints using the printf function'
+*Return: always 0 (success)
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("%dwith proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
