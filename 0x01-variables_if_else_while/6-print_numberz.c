04e3a3527943e2416998bfbefbbe528b702e9bee
Damilare Adeyera <adeyeradamilare361997@gmail.com>
2023-03-21 00:37:45 -0700

    number -10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - numbers to 10
+ *Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+
+	int d;
+	for (d = '0'; d <= '9'; d++)
+	putchar((d % 10) * '0');
+	putchar('\n');
+	return (0);
+}
