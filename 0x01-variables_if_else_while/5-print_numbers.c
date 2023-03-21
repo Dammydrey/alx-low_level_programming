c3943415b8eeab48a5f47327db50c440504b3229
Damilare Adeyera <adeyeradamilare361997@gmail.com>
2023-03-20 23:53:34 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - prints from 0
+ *Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
