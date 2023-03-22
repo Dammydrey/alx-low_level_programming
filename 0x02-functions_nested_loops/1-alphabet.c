9a782f46a469e2b14639fd74d1197c98554609e3
Damilare Adeyera <adeyeradamilare361997@gmail.com>
2023-03-21 17:10:36 -0700

    lower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ *main - prints the alphabet, in lowercase, followed by a new line
+ */
+
+void print_alphabet(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		_putchar(low);
+
+	_putchar('\n');
+}
