3eeb9a3699c27068a1ebbeb18e57e548b51de88d
Damilare Adeyera <adeyeradamilare361997@gmail.com>
2023-04-03 11:02:30 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,19 @@
 #include "main.h"
-/**_memset() function - fills the first n bytes of the memory with the constant number
-*Return: pointer to the memory area s
-*/
+/**
+ *_memset() function - fills the first n bytes of the memory area pointed to by s with the constant byte b
+*
+ *Returns: a pointer to the memory area s
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	while (n > 0)
+	while (i < n)
 	{
 		s[i] = b;
 		i++;
-		n--;
 	}
-	returns (s);
+
+	return (s);
 }
