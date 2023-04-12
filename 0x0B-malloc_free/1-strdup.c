4c52362c3ca2b0d3b7e946f566fabc89acf89424
Damilare Adeyera <adeyeradamilare361997@gmail.com>
2023-04-12 02:57:37 -0700

    task 5

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,16 +17,13 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-
-	for (a = 0; str[a] != '\0'; a++);
+	for (a = 0; str[a] != '\0'; a++)
+		;
 	s = (char *)  malloc((a + 1) * sizeof(char));
-	
 	if (s == NULL)
 		return (NULL);
-	
 	for (b = 0; b < a ; b++)
 		s[b] = str[b];
-	
 	s[a + 1] = '\0';
 	return (s);
 }
